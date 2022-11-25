e06f61d0e2ae43d42ed549d69d594a8402517422
Olga Dolgova <olgd@equinor.com>
2022-11-25 14:07:46 +0100

    refactoring

--- UnitTests.cs
+++ UnitTests.cs
@@ -93,6 +93,7 @@ namespace TestProject
         [InlineData(6, 2, 3, 4, 5, 20)]
         [InlineData(2, 3, 4, 5, 6, 20)]
         [InlineData(1, 2, 2, 4, 5, 0)]
+        [InlineData(1, 2, 3, 4, 5, 0)]
         public void GivenLargeStraight_WhenFiveDice_ThenScoreTwentyIfLargeStraight(int d1, int d2, int d3, int d4, int d5, int expected)
         {
             var diceCalculator = new DiceCalculator(d1, d2, d3, d4, d5);
